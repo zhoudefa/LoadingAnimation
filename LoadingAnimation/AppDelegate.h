ff723ad6f85fe7f80c5630e38563199277450907
Macsyf <syf@macsyftekimac-mini.local>
2017-02-27 14:12:12 +0800

    Initial Commit

---
+++ LoadingAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadingAnimation
+//
+//  Created by Macsyf on 17/2/27.
+//  Copyright © 2017年 ZhouDeFa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadingAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingAnimation
+//
+//  Created by Macsyf on 17/2/27.
+//  Copyright © 2017年 ZhouDeFa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

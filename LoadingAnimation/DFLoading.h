9309702f0d7185af0d1c6beace4a666d05264101
周德发 <zhoudefa@zhoudefadeMacBook-Pro.local>
2017-06-27 17:02:27 +0800

    first commit

---
+++ LoadingAnimation/DFLoading.h
@@ -0,0 +1,13 @@
+//
+//  DFLoading.h
+//  LoadingAnimation
+//
+//  Created by Macsyf on 17/2/27.
+//  Copyright © 2017年 ZhouDeFa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFLoading : UIView
+
+@end
---
+++ LoadingAnimation/DFLoading.m
@@ -0,0 +1,21 @@
+//
+//  DFLoading.m
+//  LoadingAnimation
+//
+//  Created by Macsyf on 17/2/27.
+//  Copyright © 2017年 ZhouDeFa. All rights reserved.
+//
+
+#import "DFLoading.h"
+
+@implementation DFLoading
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
